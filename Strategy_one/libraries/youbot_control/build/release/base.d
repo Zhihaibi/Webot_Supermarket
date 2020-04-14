c1bb1564b3aaa30035555362708e519b9c8b28b5
Alanaab <978037153@qq.com>
2020-04-14 22:35:19 +0800

    Final version

---
+++ Strategy_one/libraries/youbot_control/build/release/base.d
@@ -0,0 +1,8 @@
+build/release/base.o: src/base.c include/base.h \
+ D:\Webots/include/controller/c/webots/types.h include/tiny_math.h \
+ D:\Webots/include/controller/c/webots/compass.h \
+ D:\Webots/include/controller/c/webots/types.h \
+ D:\Webots/include/controller/c/webots/gps.h \
+ D:\Webots/include/controller/c/webots/motor.h \
+ D:\Webots/include/controller/c/webots/robot.h \
+ D:\Webots/include/controller/c/webots/nodes.h
---
+++ Strategy_one/libraries/youbot_control/build/release/tiny_math.d
@@ -0,0 +1,2 @@
+build/release/tiny_math.o: src/tiny_math.c include/tiny_math.h \
+ D:\Webots/include/controller/c/webots/types.h
